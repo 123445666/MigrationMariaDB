565349fd5533887fa2874d4fa969857b8751d8dd
funaway89 <funaway89@gmail.com>
2022-10-10 22:20:56 +0200

    commit

---
+++ DemoMariaDB/Model/AccountContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoMariaDB.Model
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<OnlineAccount> OnlineAccounts { get; set; }
+    }
+}
